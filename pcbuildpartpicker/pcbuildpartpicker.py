24e9fa1b24d4c2bc859770631f2d5b5b877024b3
Ralph Depping <github@depping.me>
2021-04-05 19:48:05 +0100

    One at a time swap out working

--- pcbuildpartpicker/pcbuildpartpicker.py
+++ pcbuildpartpicker/pcbuildpartpicker.py
@@ -10,6 +10,7 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--parts-file', type=str, help='json file containg parts to pick from')
     parser.add_argument('-b', '--budget', type=int, help='budget in euros')
+    parser.add_argument('-s', '--sort', type=str, help='sort by', default='cost')
     args = parser.parse_args()
 
     return args
@@ -27,19 +28,19 @@ def main():
         original_parts = load_data_from_csv(args.parts_file)
         print(f'Parts to pick from \n{original_parts}')
 
-        add_exp_score_by_cost_to(original_parts)
-        print(f'Parts with experience score by cost \n{original_parts}')
+        # add_exp_score_by_cost_to(original_parts)
+        # print(f'Parts with experience score by cost \n{original_parts}')
 
-        selected_parts = select_parts_based_on(original_parts, 'exp/cost')
-        print_results('Best Value', selected_parts)
+        # selected_parts = select_parts_based_on(original_parts, 'exp/cost')
+        # print_results('Best Value', selected_parts)
 
         reset_selected_parts(original_parts)
         selected_parts = select_parts_based_on(original_parts, 'experience_score')
         print_results('Maximum Experience Score', selected_parts)
 
-        get_in_budget(original_parts, args.budget)
+        selected_parts = get_in_budget(original_parts, args.budget, args.sort)
 
-        # print_results('All Parts', original_parts)
+        print_results('Selected Parts', selected_parts)
 
         sys.exit(0)
     except Exception as e:
@@ -59,39 +60,44 @@ def print_results(use_case, selected_parts):
     print(f"Total Experience Score: {selected_parts['experience_score'].sum()}\n")
 
 
-def get_in_budget(parts, budget):
-    if in_budget(parts, budget):
-        return
-    parts.sort_values(['selected', 'experience_score'], ascending=[False, False], inplace=True)
-    parts.reset_index(drop=True, inplace=True)
-    # print_results("SORTED by selected and experience_score", parts)
+def get_in_budget(original_parts, budget, sort='experience_score'):
+    original_parts.sort_values(by=['selected', 'experience_score'], ascending=[False, True], inplace=True, ignore_index=True)
+    if in_budget(original_parts, budget):
+        return original_parts.loc[original_parts['selected'] == True]
     attempts = 0
-    while not in_budget(parts, budget) and attempts < 6:
-        parts.at[attempts, 'selected'] = False
-        category = parts.at[attempts, 'category']
-        deselected_part = parts.at[attempts, 'name']
+    in_budget_flag = False
+    while not in_budget_flag and attempts < 6: # 6 categories
+        selected_parts = original_parts.loc[original_parts['selected'] == True]
+        category = selected_parts.at[attempts, 'category']
+        not_selected_df = original_parts[(original_parts['selected'] == False) & (original_parts['category'] == category)].sort_values(by=['experience_score'], ascending=False)
+        selected_parts.at[attempts, 'selected'] = False
+        deselected_part = original_parts.at[attempts, 'name']
         print('#' * 30)
-        print(f"Attempt: {attempts}, Category: {category}")
-        print('#' * 30)
-        for index, row in parts.iterrows():
-            if row['category'] == category and row['name'] != deselected_part:
-                parts.at[index, 'selected'] = True
-                new_part = row
+        print(f"Attempt: {attempts} for Category: {category}")
+        for index, row in not_selected_df.iterrows():
+            print(f"\t- Removing {deselected_part} adding {original_parts.loc[index]['name']}")
+            selected_parts = selected_parts.append(row)
+            selected_parts.at[index, 'selected'] = True
+            if in_budget(selected_parts, budget):
+                in_budget_flag = True
                 break
-        print(f"Check if {new_part['name']} costing {new_part['cost']} instead of {deselected_part} works")
+            else:
+                selected_parts = selected_parts.drop(index)
+        print('#' * 30)
         attempts += 1
+    return selected_parts.loc[selected_parts['selected'] == True]
 
 
 def in_budget(parts, budget):
     selected_parts = parts.loc[parts['selected'] == True]
-    # print_results("Budget attempt", selected_parts)
+    print_results("Budget Match Attempt", selected_parts)
     cost = selected_parts['cost'].sum()
     if cost > budget:
         print(f'Cost: €{cost} > Budget: €{budget}')
         return False
     else:
         print(f'Cost: €{cost} within budget: €{budget}')
-        print_results("", selected_parts)
+        print_results("Within Budget Pick", selected_parts)
         return True
 
 
