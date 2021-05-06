017ce97f02031750717bec7831cbfe702315e3f9
Ralph Depping <github@depping.me>
2021-05-06 23:29:35 +0100

    add some very basic unit tests and run black formatter over code

--- pcbuildpartpicker/pcbuildpartpicker.py
+++ pcbuildpartpicker/pcbuildpartpicker.py
@@ -5,15 +5,23 @@ import sys
 import pandas
 
 
-def get_args() -> argparse.Namespace:
+def get_args(args) -> argparse.Namespace:
     """Get command-line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--parts-file', type=str, help='json file containg parts to pick from')
-    parser.add_argument('-b', '--budget', type=int, help='budget in euros')
-    parser.add_argument('-s', '--sort', type=str, help='sort by', default='exp/cost')
-    args = parser.parse_args()
+    parser.add_argument(
+        "-f",
+        "--parts-file",
+        type=str,
+        help="json file containing parts to pick from",
+        required=True,
+    )
+    parser.add_argument(
+        "-b", "--budget", type=int, help="budget in euros", required=True
+    )
+    parser.add_argument("-s", "--sort", type=str, help="sort by", default="exp/cost")
+    parsed_args = parser.parse_args(args)
 
-    return args
+    return parsed_args
 
 
 def load_data_from_csv(parts_file: str):
@@ -22,82 +30,88 @@ def load_data_from_csv(parts_file: str):
 
 
 def main():
-    args = get_args()
-    print(f'Budget €{args.budget:0,.2f}\n')
+    args = get_args(sys.argv[1:])
+    print(f"Budget €{args.budget:0,.2f}\n")
     summary_printer = part_picker_summary_print(args.budget)
     try:
         original_parts = load_data_from_csv(args.parts_file)
-        print(f'Parts to pick from \n{original_parts}')
+        print(f"Parts to pick from \n{original_parts}")
 
-        add_exp_score_by_cost_to(original_parts)
-        print(f'Parts with experience score by cost \n{original_parts}')
+        add_additional_columns(original_parts)
+        print(f"Parts with experience score by cost \n{original_parts}")
 
-        selected_parts = select_parts_based_on(original_parts, 'exp/cost')
-        summary_printer('Best Value', selected_parts)
+        selected_parts = select_parts_based_on(original_parts, "exp/cost")
+        summary_printer("Best Value", selected_parts)
 
         reset_selected_parts(original_parts)
-        selected_parts = select_parts_based_on(original_parts, 'experience_score')
-        summary_printer('Maximum Experience Score Pick', selected_parts)
+        selected_parts = select_parts_based_on(original_parts, "experience_score")
+        summary_printer("Maximum Experience Score Pick", selected_parts)
 
         selected_parts = select_for_budget(original_parts, args.budget, args.sort)
 
         if selected_parts is None:
-            print(f'Failed to find parts in budget {args.budget}')
+            print(f"Failed to find parts in budget {args.budget}")
         else:
-            summary_printer('Selected Parts', selected_parts)
+            summary_printer("Selected Parts", selected_parts)
 
         sys.exit(0)
     except Exception as e:
-        print(f'exception: {e}')
+        print(f"exception: {e}")
         sys.exit(1)
 
 
 def reset_selected_parts(parts):
-    parts.loc[:, 'selected'] = False
+    parts.loc[:, "selected"] = False
 
 
 def part_picker_summary_print(budget):
     def print_part_picker_summary(use_case, selected_parts):
-        print('\n\n')
-        print('#' * 10 + f' {use_case} ' + '#' * 10)
-        print(f'{selected_parts}')
-        print('#' * 40)
+        print("\n\n")
+        print("#" * 10 + f" {use_case} " + "#" * 10)
+        print(f"{selected_parts}")
+        print("#" * 40)
         print(f"Total Budget: €{budget:0,.2f}")
-        costs = selected_parts['cost'].sum()
+        costs = selected_parts["cost"].sum()
         print(f"Total Cost: €{costs :0,.2f}")
-        print(f"Remaining Budget: €{budget-costs:0,.2f}")
+        print(f"Remaining Budget: €{budget - costs:0,.2f}")
         print(f"Total Experience Score: {selected_parts['experience_score'].sum()}")
-        print('#' * 40)
-        print('\n\n')
+        print("#" * 40)
+        print("\n\n")
+
     return print_part_picker_summary
 
 
 def select_for_budget(original_parts, budget, sort):
-    original_parts.sort_values(by=['selected', sort], ascending=[False, True], inplace=True,
-                               ignore_index=True)
+    original_parts.sort_values(
+        by=["selected", sort], ascending=[False, True], inplace=True, ignore_index=True
+    )
     if in_budget(original_parts, budget):
-        return original_parts.loc[original_parts['selected'] == True]
+        return original_parts.loc[original_parts["selected"] == True]
     category_idx = 0
     in_budget_flag = False
-    selected_parts = original_parts.loc[original_parts['selected'] == True]
+    selected_parts = original_parts.loc[original_parts["selected"] == True]
     while not in_budget_flag and category_idx < 6:  # 6 categories
-        category = selected_parts.at[category_idx, 'category']
+        category = selected_parts.at[category_idx, "category"]
         not_selected_parts = get_alternative_parts(category, original_parts)
-        deselected_part = selected_parts.at[category_idx, 'name']
+        deselected_part = selected_parts.at[category_idx, "name"]
         selected_parts = selected_parts.drop(category_idx)
         prev_not_selected_idx = None
         for not_selected_parts_idx, row in not_selected_parts.iterrows():
             if prev_not_selected_idx:
-                selected_parts = selected_parts.drop(prev_not_selected_idx) # We are trying again, so drop the last selected
+                selected_parts = selected_parts.drop(
+                    prev_not_selected_idx
+                )  # We are trying again, so drop the last selected
             prev_not_selected_idx = not_selected_parts_idx
-            print('#' * 40)
+            print("#" * 40)
             print(f"Category Part Swap: {category}")
             print(f"\t- Removing: {deselected_part}")
-            print(f"\t- Adding: {not_selected_parts.loc[not_selected_parts_idx]['name']}")
+            print(
+                f"\t- Adding: {not_selected_parts.loc[not_selected_parts_idx]['name']}"
+            )
             selected_parts = selected_parts.append(row)
-            selected_parts.at[not_selected_parts_idx, 'selected'] = True
+            selected_parts.at[not_selected_parts_idx, "selected"] = True
             if in_budget(selected_parts, budget):
-                return selected_parts.loc[selected_parts['selected'] == True]
+                return selected_parts.loc[selected_parts["selected"] == True]
             # print('#' * 40)
         category_idx += 1
     return None
@@ -105,19 +119,19 @@ def select_for_budget(original_parts, budget, sort):
 
 def get_alternative_parts(category, original_parts):
     return original_parts[
-        (original_parts['selected'] == False) & (original_parts['category'] == category)].sort_values(
-        by=['experience_score'], ascending=False)
+        (original_parts["selected"] == False) & (original_parts["category"] == category)
+    ].sort_values(by=["experience_score"], ascending=False)
 
 
 def in_budget(parts, budget):
-    selected_parts = parts.loc[parts['selected'] == True]
+    selected_parts = parts.loc[parts["selected"] == True]
     # print_results("Budget Match Attempt", selected_parts)
-    cost = selected_parts['cost'].sum()
+    cost = selected_parts["cost"].sum()
     if cost > budget:
-        print(f'Cost: €{cost} > Budget: €{budget}')
+        print(f"Cost: €{cost} > Budget: €{budget}")
         return False
     else:
-        print(f'Cost: €{cost} within budget: €{budget}')
+        print(f"Cost: €{cost} within budget: €{budget}")
         # print_results("Within Budget Pick", selected_parts)
         return True
 
@@ -126,15 +140,15 @@ def select_parts_based_on(parts, sort_by):
     parts.sort_values(by=[sort_by], inplace=True, ascending=False)
     picked_part_category_tracker = []
     for index, row in parts.iterrows():
-        if row['category'] not in picked_part_category_tracker:
-            picked_part_category_tracker.append(row['category'])
-            parts.at[index, 'selected'] = True
-    return parts.loc[parts['selected'] == True]
+        if row["category"] not in picked_part_category_tracker:
+            picked_part_category_tracker.append(row["category"])
+            parts.at[index, "selected"] = True
+    return parts.loc[parts["selected"] == True]
 
 
-def add_exp_score_by_cost_to(parts):
-    parts['selected'] = False
-    parts['exp/cost'] = parts['experience_score'] / parts['cost']
+def add_additional_columns(parts):
+    parts["selected"] = False
+    parts["exp/cost"] = parts["experience_score"] / parts["cost"]
 
 
 if __name__ == "__main__":
--- pcbuildpartpicker/test_pcbuildpartpicker.py
+++ pcbuildpartpicker/test_pcbuildpartpicker.py
@@ -1,7 +1,273 @@
+from pandas import DataFrame
+
 import pcbuildpartpicker
 
+selected_parts_test_data = {
+    "category": {
+        0: "case",
+        1: "storage",
+        2: "motherboard",
+        3: "memory",
+        4: "graphics",
+        5: "cpu",
+    },
+    "name": {
+        0: "very-base case",
+        1: "base storage 1",
+        2: "mid mb 2",
+        3: "base memory 1",
+        4: "mid graphics 2",
+        5: "high-end cpu 3",
+    },
+    "cost": {0: 5, 1: 40, 2: 75, 3: 100, 4: 120, 5: 300},
+    "experience_score": {0: 2, 1: 5, 2: 8, 3: 8, 4: 12, 5: 30},
+    "selected": {0: True, 1: True, 2: True, 3: True, 4: True, 5: True},
+    "exp/cost": {0: 0.4, 1: 0.125, 2: 0.10666666666666667, 3: 0.08, 4: 0.1, 5: 0.1},
+}
 
-def test_main_prints_hello_world(capsys):
-    pcbuildpartpicker.main()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello World!\n"
+original_parts_test_data = {
+    "category": {
+        0: "case",
+        1: "case",
+        2: "case",
+        3: "case",
+        4: "motherboard",
+        5: "motherboard",
+        6: "motherboard",
+        7: "cpu",
+        8: "cpu",
+        9: "cpu",
+        10: "graphics",
+        11: "graphics",
+        12: "graphics",
+        13: "storage",
+        14: "storage",
+        15: "storage",
+        16: "memory",
+        17: "memory",
+        18: "memory",
+    },
+    "name": {
+        0: "base case 1",
+        1: "mid case 2",
+        2: "high-end case 3",
+        3: "very-base case",
+        4: "base mb 1",
+        5: "mid mb 2",
+        6: "high-end mb 3",
+        7: "base cpu 1",
+        8: "mid cpu 2",
+        9: "high-end cpu 3",
+        10: "base graphics 1",
+        11: "mid graphics 2",
+        12: "high-end graphics 3",
+        13: "base storage 1",
+        14: "mid storage 2",
+        15: "high-end storage 3",
+        16: "base memory 1",
+        17: "mid memory 2",
+        18: "high-end memory 3",
+    },
+    "cost": {
+        0: 30,
+        1: 50,
+        2: 75,
+        3: 5,
+        4: 50,
+        5: 75,
+        6: 120,
+        7: 150,
+        8: 200,
+        9: 300,
+        10: 80,
+        11: 120,
+        12: 200,
+        13: 40,
+        14: 80,
+        15: 120,
+        16: 100,
+        17: 150,
+        18: 200,
+    },
+    "experience_score": {
+        0: 3,
+        1: 6,
+        2: 8,
+        3: 2,
+        4: 5,
+        5: 8,
+        6: 12,
+        7: 12,
+        8: 20,
+        9: 30,
+        10: 8,
+        11: 12,
+        12: 16,
+        13: 5,
+        14: 7,
+        15: 9,
+        16: 8,
+        17: 12,
+        18: 14,
+    },
+}
+
+original_parts_selected_by_experience_score = {
+    "category": {
+        9: "cpu",
+        8: "cpu",
+        12: "graphics",
+        18: "memory",
+        6: "motherboard",
+        7: "cpu",
+        11: "graphics",
+        17: "memory",
+        15: "storage",
+        5: "motherboard",
+        10: "graphics",
+        2: "case",
+        16: "memory",
+        14: "storage",
+        1: "case",
+        4: "motherboard",
+        13: "storage",
+        0: "case",
+        3: "case",
+    },
+    "name": {
+        9: "high-end cpu 3",
+        8: "mid cpu 2",
+        12: "high-end graphics 3",
+        18: "high-end memory 3",
+        6: "high-end mb 3",
+        7: "base cpu 1",
+        11: "mid graphics 2",
+        17: "mid memory 2",
+        15: "high-end storage 3",
+        5: "mid mb 2",
+        10: "base graphics 1",
+        2: "high-end case 3",
+        16: "base memory 1",
+        14: "mid storage 2",
+        1: "mid case 2",
+        4: "base mb 1",
+        13: "base storage 1",
+        0: "base case 1",
+        3: "very-base case",
+    },
+    "cost": {
+        9: 300,
+        8: 200,
+        12: 200,
+        18: 200,
+        6: 120,
+        7: 150,
+        11: 120,
+        17: 150,
+        15: 120,
+        5: 75,
+        10: 80,
+        2: 75,
+        16: 100,
+        14: 80,
+        1: 50,
+        4: 50,
+        13: 40,
+        0: 30,
+        3: 5,
+    },
+    "experience_score": {
+        9: 30,
+        8: 20,
+        12: 16,
+        18: 14,
+        6: 12,
+        7: 12,
+        11: 12,
+        17: 12,
+        15: 9,
+        5: 8,
+        10: 8,
+        2: 8,
+        16: 8,
+        14: 7,
+        1: 6,
+        4: 5,
+        13: 5,
+        0: 3,
+        3: 2,
+    },
+    "selected": {
+        9: True,
+        8: False,
+        12: True,
+        18: True,
+        6: True,
+        7: False,
+        11: False,
+        17: False,
+        15: True,
+        5: False,
+        10: False,
+        2: True,
+        16: False,
+        14: False,
+        1: False,
+        4: False,
+        13: False,
+        0: False,
+        3: False,
+    },
+    "exp/cost": {
+        9: 0.1,
+        8: 0.1,
+        12: 0.08,
+        18: 0.07,
+        6: 0.1,
+        7: 0.08,
+        11: 0.1,
+        17: 0.08,
+        15: 0.075,
+        5: 0.10666666666666667,
+        10: 0.1,
+        2: 0.10666666666666667,
+        16: 0.08,
+        14: 0.0875,
+        1: 0.12,
+        4: 0.1,
+        13: 0.125,
+        0: 0.1,
+        3: 0.4,
+    },
+}
+
+
+def test_print_summary():
+    budget = 1000
+    summary_print = pcbuildpartpicker.part_picker_summary_print(budget)
+    summary_print("test use case", DataFrame.from_dict(selected_parts_test_data))
+
+
+def test_add_columns():
+    test_parts_df = DataFrame.from_dict(
+        {"experience_score": [10, 20, 30], "cost": [2, 4, 8]}
+    )
+    assert "selected" not in test_parts_df.columns
+    assert "exp/cost" not in test_parts_df.columns
+
+    pcbuildpartpicker.add_additional_columns(test_parts_df)
+
+    assert "selected" in test_parts_df.columns
+    assert "exp/cost" in test_parts_df.columns
+
+
+def test_unique_categories_selected_for_given_budget():
+    budget = 1000
+    selected_parts = pcbuildpartpicker.select_for_budget(
+        DataFrame.from_dict(original_parts_selected_by_experience_score),
+        budget=budget,
+        sort="exp/cost",
+    )
+    assert len(selected_parts["selected"]) == 6
+    assert selected_parts["cost"].sum() <= budget
+    assert selected_parts["category"].is_unique
